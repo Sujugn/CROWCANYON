9e8c515eebbc9f745912aa17191602e2314315cc
Sujugn <pepsi030@naver.com>
2023-07-10 17:46:15 +0900

    DD

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -14,7 +14,13 @@ export default function Detail(props) {
   });
 
   return (
-    <div style={{ display: "flex" }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <img src={bests[id].image} alt="detail_img" style={{ width: 500 }} />
 
       <section>
@@ -23,29 +29,54 @@ export default function Detail(props) {
 
         <div>
           <span>상세설명</span>
-          <p>{bests[id].detail}</p>
+          <p style={{ fontSize: 12 }}>{bests[id].detail}</p>
         </div>
 
         <div>
           <span>배송</span>
           <p>￦3,000 (￦50,000 이상 구매 시 무료) </p>
         </div>
-        <button
-          style={{ width: 300 }}
-          onClick={() => {
-            dispatch(
-              addItem({
-                id: bests[id].id,
-                title: bests[id].title,
-                count: 1,
-                price: bests[id].price,
-                pricetotal: bests[id].price,
-              })
-            );
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
           }}
         >
-          장바구니에 담기
-        </button>
+          <button
+            style={{
+              width: 300,
+              height: 40,
+              backgroundColor: "black",
+              color: "white",
+              border: "1px solid black",
+              borderRight: "none",
+              marginRight: 10,
+            }}
+            onClick={() => {
+              dispatch(
+                addItem({
+                  id: bests[id].id,
+                  title: bests[id].title,
+                  count: 1,
+                  price: bests[id].price,
+                  pricetotal: bests[id].price,
+                })
+              );
+            }}
+          >
+            Add to Cart
+          </button>
+          <button
+            style={{
+              width: 300,
+              height: 40,
+              backgroundColor: "white",
+              border: "1px solid black",
+            }}
+          >
+            구매하기
+          </button>
+        </div>
       </section>
     </div>
   );
